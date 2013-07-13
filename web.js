944b699a170324a3ebbd08c749d3f2352282a540
Jeri Steele <TXPioneer@TXPioneer.com>
2013-07-13 11:26:59 -0500

    Version from Mac
    Working File reader

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
-var express = require('express');
+/* Startup engine */
+
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+app.get('/', function(request,response) {
+    var fs = require('fs');
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+var buf = new Buffer(fs.readFileSync(__dirname + '\/index.html'),'utf-8');
+response.send(buf.toString());
 });
+var port = process.env.PORT;
+app.listen(port, function() {
+    console.log("Listening on " + port);
+}).listen(process.env.PORT, process.env.IP);
